aca569aa8bee86cd6097fc9022dc8209a9a31eae
Rmbo <ramask8179@gmail.com>
2026-05-16 12:09:02 +0530

    added controller

---
+++ src/main/java/todo/tutorials/config/HomeController.java
@@ -0,0 +1,13 @@
+package todo.tutorials.config;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Payment Integration System API Running";
+    }
+}
